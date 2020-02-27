c5052fa589c3629391bac06f39e1fc6b545103dd
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-02-27 21:35:11 +0530

    odd no's

---
+++ a_2.c
@@ -0,0 +1,17 @@
+#print the odd no's in an array
+
+#include<stdio.h>
+int main()
+{
+    int a[20],i;
+    printf("Enter ten no's: ");
+    for(i=0; i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        if(a[i]%2==1)
+            printf("%d ",a[i]);
+    }
+}
